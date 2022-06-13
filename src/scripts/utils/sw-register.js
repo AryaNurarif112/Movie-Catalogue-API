c8c1e831e43ad38c86c4a6bab1fe83c7a96c4549
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-13 21:16:10 +0700

    first commit movie-catalogue

---
+++ src/scripts/utils/sw-register.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+
+const swRegister = async () => {
+  if ('serviceWorker' in navigator) {
+    await runtime.register();
+  }
+};
+
+export default swRegister;
